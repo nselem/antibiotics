2231e97a830c8de7b0caad96467bf36bf7719046
nselem <nselem35@hotmail.com>
2019-12-21 03:25:07 -0600

    lactam

--- data/antiSMASH-GBK-Antibioticos/converter.pl
+++ data/antiSMASH-GBK-Antibioticos/converter.pl
@@ -1,19 +1,29 @@
 use Bio::SeqIO;
+#use Bio::Annotation; 
+#use Bio::AnnotationCollection;
+
 my $file=$ARGV[0];
 
 $seqio_obj = Bio::SeqIO->new(-file => "$file",  -format => "genbank" );
 
 $seqio_obj = Bio::SeqIO->new(-file => "$file",  -format => "genbank" );
 my $out= Bio::SeqIO->new(-file=> ">$file\.faa",-format=> 'Fasta');
-my $seq_object = $seqio_obj->next_seq;
+my $seq_object = $seqio_obj->next_seq; 
+#my $txt=open(FILE,">$file.txt") or die $!;
+#my $anno_collection = $seq_obj->annotation;
 
-my $cont=1;
 for my $feat_object ($seq_object->get_SeqFeatures) {
     if ($feat_object->primary_tag eq "CDS") {
         if ($feat_object->has_tag('locus_tag') and $feat_object->has_tag('translation') ) {
             for my $val ($feat_object->get_tag_values('locus_tag')) {
                 for my $prot ($feat_object->get_tag_values('translation')) {
 #                       print ">$val\n$prot\n";
+#			 my $accesion = $anno_collection->get_Annotations('accession');
+                        my $organism = $feat_object->get_tag_value('organism');
+                       # print FILE "$accession\t$prot\t$accession\t$organism";
+                        #print  "$accession\ti";
+			print "$prot\t$organism\n";
+
               my $seq = Bio::Seq->new(-seq => $prot, -display_id => "$val");
 		$out->write_seq($seq);	
 
